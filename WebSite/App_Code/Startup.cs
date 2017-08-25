4220e3c7cb763053a08026185070e3418b6f54ac
JSMITH <JSMITH@JSMITHDEV>
2017-08-25 10:55:14 -0700

    Add project files.

---
+++ WebSite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testweb3.Startup))]
+namespace testweb3
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
